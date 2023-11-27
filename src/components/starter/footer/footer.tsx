adf159a59225e4fdaa3e8e6ff1dc5cdff91f2d42
John Cornyn <JMIHC@users.noreply.github.com>
2023-11-27 12:37:40 -0800

    add footer

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,17 +1,14 @@
 import { component$ } from "@builder.io/qwik";
-import { useServerTimeLoader } from "~/routes/layout";
-import styles from "./footer.module.css";
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
 
   return (
     <footer>
-      <div class="container">
-        <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
-          <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
+      <div class="container flex justify-center my-20">
+        <a href="https://www.fullcirclelearning.org/" target="_blank">
+          <span>Made with ♡ by Full Circle Learning</span>
+          <span class="mx-2">|</span>
+          <span>2023</span>
         </a>
       </div>
     </footer>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,26 +4,35 @@ import '../global.css';
 
 export default component$(() => {
   return (
-    <>
-          <div class="flex justify-center mt-20">
+    <div class="space-y-10 mx-6">
+          <div class="flex justify-center">
             <img width="691" height="194" alt="Educators consulting around a table" src="images/peopleAroundTable.jpg" />
           </div>
-          <div class="flex justify-center mt-20">
+          <div class="flex justify-center">
           <h1 class="text-4xl font-bold">
             <div class="text-center">Full-Circle Learning:</div>
             <div class="text-center">Enhancing Pedagogy, Practice, Purpose</div>
 </h1>
           </div>
           <div class="flex justify-center">
-          <div class="flex flex-col mx-6 md:mx-20 prose lg:prose-xl mt-20">
-            <p class="">Education typically focuses on what to learn or how to learn. Full-Circle Learning also emphasizes why to learn.</p>
+          <div class="container mx-auto max-w-4xl">
+      <div class="flex flex-col md:flex-row gap-4">
+        <div class="md:w-1/2">
+          <img src="images/studentStudying.jpg" width="398" height="300" alt="Student studies at their desk" class="max-w-lg h-auto" />
+        </div>
+        <div class="md:w-1/2 prose lg:prose-xl">
+        <p class="">Education typically focuses on what to learn or how to learn. Full-Circle Learning also emphasizes why to learn.</p>
             <p class="">This research-based model of education cultivates the instinct to connect and to create, to balance curiosity with compassion, to problem-solve with humanitarian intent, to improve social cohesion, motivation and behavior.</p>
-            <p>Learners broaden their sphere of influence as change agents in the world. Their learning influences the wellbeing of other living beings. This transformative impact, in turn, renews the human desire to learn.</p>
+        </div>
+      </div>
+      <div class="prose lg:prose-xl">
+      <p>Learners broaden their sphere of influence as change agents in the world. Their learning influences the wellbeing of other living beings. This transformative impact, in turn, renews the human desire to learn.</p>
             <p>Explore this site to discover how you and your colleagues can intensify your own sense of purpose by infusing purpose into all learning.</p>
-          </div>
+      </div>
+    </div>
           </div>
 
-    </>
+    </div>
   );
 });
 
