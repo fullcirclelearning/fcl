0c1dea59118806644f66c94368a97a04a0970ede
John Cornyn <JMIHC@users.noreply.github.com>
2023-11-27 12:38:29 -0800

    add space

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -25,7 +25,7 @@ export default component$(() => {
             <p class="">This research-based model of education cultivates the instinct to connect and to create, to balance curiosity with compassion, to problem-solve with humanitarian intent, to improve social cohesion, motivation and behavior.</p>
         </div>
       </div>
-      <div class="prose lg:prose-xl">
+      <div class="prose lg:prose-xl mt-6">
       <p>Learners broaden their sphere of influence as change agents in the world. Their learning influences the wellbeing of other living beings. This transformative impact, in turn, renews the human desire to learn.</p>
             <p>Explore this site to discover how you and your colleagues can intensify your own sense of purpose by infusing purpose into all learning.</p>
       </div>
